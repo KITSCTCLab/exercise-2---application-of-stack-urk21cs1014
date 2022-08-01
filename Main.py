414fd51bfc82ead8186376d51f5b902a10d4cbcc
urk21cs1014 <110385851+urk21cs1014@users.noreply.github.com>
2022-08-01 15:10:23 +0530

    submission-2

--- Main.py
+++ Main.py
@@ -1,36 +1,92 @@
-import os
-class Stack:
-    def __init__(self, size):
-        self.items = []
-        self.size = size
+class Evaluate:
+ #This class validates and evaluate postfix expression.
+ #Attributes:
+ #   top: An integer which denotes the index of the element at the top of the stack currently.
+ #   size_of_stack: An integer which represents the size of stack.
+ #   stack: A List which acts as a Stack.
 
-    def is_empty(self):
-        if len(self.items) ==0:
-            return True
 
-    def is_full(self):
-        if len(self.items)==self.size:
-            return True
+  def __init__(self, size):
+  #Inits Evaluate with top, size_of_stack and stack.
+  #  Arguments:
+  #    size_of_stack: An integer to set the size of stack.
+    self.top = -1
+    self.size_of_stack = size
+    self.stack = []
 
-    def push(self, data):
-        if not self.is_full():
-            self.items.append(data)
 
-    def pop(self):
-        if not self.is_empty():
-            self.items.pop()
-            
-    def status(self):
-        for elements in self.items:
-            print(elements)
+  def isEmpty(self):
+    #Check whether the stack is empty.
+    #Returns:
+    # True if it is empty, else returns False
+      return len(self.stack) == 0
+
+
+  def pop(self):
+    #Do pop operation if the stack is not empty.
+    #Returns:
+    # The data which is popped out if the stack is not empty.
+    if not self.isEmpty():
+      self.top -= 1
+      return self.stack.pop(-1)
+   
+
+
+  def push(self, operand):
+   #Push the operand to stack if the stack is not full.
+   #Arguments:
+   # operand: The operand to be pushed.
+    if len(self.stack) < self.size_of_stack:
+      self.top += 1
+      self.stack.append(operand)
+     
+
+
+  def validate_postfix_expression(self, expression):
+    #Check whether the expression is a valid postfix expression.
+    #Arguments:
+    #  expression: A list which represents the expression to be validated.
+    #Returns:
+    #  True if the expression is valid, else returns False
+    operands = [element for element in expression if element.isdigit()]
+    operators = [element for element in expression  if element in ["+", "-", "*", "/", "^"]]
+    if (len(operands) + len(operators)) == len(expression) and len(operands) == len(operators) + 1:
+        return expression[0] not in operators and expression[1] not in operators
+
+
+  def evaluate_postfix_expression(self, expression):
+    #Evaluate the postfix expression
+    #Arguments:
+    #    expression: A list which represents the the expression to be evaluated
+    #Returns:
+    #    The result of evaluated postfix expression.
+    self.stack = []
+    for element in expression:
+      if element.isdigit():
+        self.push(int(element))
+      elif element in ["+", "-", "*", "/", "^"]:
+        if element == "+":
+          result = self.stack[-2] + self.stack[-1]
+        elif element == "-":
+          result = self.stack[-2] - self.stack[-1]
+        elif element == "*":
+          result = self.stack[-2] * self.stack[-1]
+        elif element == "/":
+          result = self.stack[-2] // self.stack[-1]
+        elif element == "^":
+          result = self.stack[-2] ** self.stack[-1]
+        self.pop()
+        self.pop()
+        self.push(result)
+    return self.pop()
+   
+
 
 # Do not change the following code
-size, queries = map(int, input().rstrip().split())
-stack = Stack(size)
-for line in range(queries):
-    values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
-        stack.push(values[1])
-    elif values[0] == 2:
-        stack.pop()
-stack.status()
+postfix_expression = input()  # Read postfix expression
+tokens = postfix_expression.split()
+evaluate = Evaluate(len(tokens))
+if evaluate.validate_postfix_expression(tokens):
+    print(evaluate.evaluate_postfix_expression(tokens))
+else:
+    print('Invalid postfix expression')
